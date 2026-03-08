e9bd19fc78af23f57e6d11779084478ca0ffbce3
Ben <bennylazarussonu@gmail.com>
2026-03-08 21:24:59 +0530

    *

--- routes/workerRoutes.js
+++ routes/workerRoutes.js
@@ -58,36 +58,36 @@ function excelDateToJSDate(value) {
 }
 
 
-router.put("/temp-blood", async(req, res) => {
-  try {
-    const workers = await Worker.updateMany(
-      {},
-      {
-        $set:{
-          blood: {
-            group: "",
-            rh_factor: ""
-          }
-        }
-      });
-    res.json({ message: "Temporary blood field update successful" });
+// router.put("/temp-blood", async(req, res) => {
+//   try {
+//     const workers = await Worker.updateMany(
+//       {},
+//       {
+//         $set:{
+//           blood: {
+//             group: "",
+//             rh_factor: ""
+//           }
+//         }
+//       });
+//     res.json({ message: "Temporary blood field update successful" });
 
-    const preemps = await PreEmployment.updateMany(
-      {},
-      {
-        $set:{
-          blood: {
-            status: "Not Done",
-            group: "",
-            rh_factor: ""
-          }
-        }
-      });
-    res.json({ message: "Temporary blood field update successful" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-})
+//     const preemps = await PreEmployment.updateMany(
+//       {},
+//       {
+//         $set:{
+//           blood: {
+//             status: "Not Done",
+//             group: "",
+//             rh_factor: ""
+//           }
+//         }
+//       });
+//     res.json({ message: "Temporary blood field update successful" });
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// })
 
 
 router.get("/", protect, allowRoles("ADMIN", "DOCTOR", "EMPLOYEE"), async (req, res) => {
